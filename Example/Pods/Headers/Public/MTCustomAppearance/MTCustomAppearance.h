56087150c89641afbbe563528634d9ebd1eb5aa4
Alok Choudhary <ac687j@att.com>
2016-04-24 23:39:01 -0400

    updated

---
+++ Example/Pods/Headers/Public/MTCustomAppearance/MTCustomAppearance.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/MTCustomAppearance.h
---
+++ Example/Pods/Target Support Files/MTCustomAppearance/MTCustomAppearance-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MTCustomAppearance : NSObject
+@end
+@implementation PodsDummy_MTCustomAppearance
+@end
